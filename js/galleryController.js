14b961138da4644bf90a65cdf01e017210f4b11a
evyatar shlar <evyatar.szlar@gmail.com>
2025-01-09 19:57:52 +0200

    submission 21.00

--- js/galleryController.js
+++ js/galleryController.js
@@ -5,11 +5,16 @@ function renderGallery() {
     for (let i = 0; i < 17; i++) {
         strHtmls[i] = `<img src="meme-imgs/meme-imgs(square)/${i+1}.jpg" alt="" onclick="onImgSelect(${i+1})">`
     }
-    strHtmls.unshift(' <input type="file" class="file-input btn" name="image" onchange="onImgInput(event)" />')
+    strHtmls.unshift('<label for="file-upload" class="color-btn">Upload Image</label> <input type="file" class="color-input" id="file-upload" name="image" onchange="onImgInput(event)" />')
     document.querySelector('.gallery').innerHTML = strHtmls.join('')
 }
 
 function onImgSelect(idx) {
+    closeGallery()
+    setImg(idx)
+}
+
+function closeGallery(){
     const elEditor = document.querySelector('.edit-meme-layout')
     elEditor.style.display ='grid'
     const elGallery = document.querySelector('.gallery')
@@ -17,5 +22,4 @@ function onImgSelect(idx) {
     const elSaved = document.querySelector('.saved-memes')
     elSaved.style.display = 'none'
     resizeCanvas()
-    setImg(idx)
 }
--- js/memeController.js
+++ js/memeController.js
@@ -220,6 +220,7 @@ function loadImageFromInput(ev, onImageReady) {
         img.src = event.target.result
     }
     reader.readAsDataURL(ev.target.files[0])
+    closeGallery()
 }
 
 ///////////////////////////////////////////
@@ -301,17 +302,18 @@ function resizeCanvas() {
 function renderPics() {
     var pics = getPics()
     ///shold save all "line" object too
-    var strHtmls = pics.map(pic => `
+    var strHtmls = pics.map(pic => `<div class="container">
   <img src="${pic.pic}" alt="" onclick="onSelectPic(this)">
   <button class="btn" onclick="onRemovePic('${pic.id}')">x</button>
-`)
+    </div>`)
     document.querySelector('.saved-memes').innerHTML = strHtmls.join('')
 }
 
 function onSavePic() {
     ///shold save all "line" object too
     const imgContent = gElCanvas.toDataURL('image/jpeg')
-    addPic(imgContent)
+    var meme = getMeme()
+    addPic(imgContent, meme)
     renderPics()
     onSaved()
 }
@@ -323,8 +325,30 @@ function onRemovePic(picId) {
 
 function onSelectPic(elImg) {
     ///shold save all "line" object too {renderMeme()}
-    renderImg(elImg)
+
+    renderMeme()
 }
 
+
+
+function renderGallery() {
+    var strHtmls = []
+    for (let i = 0; i < 17; i++) {
+        strHtmls[i] = `<img src="meme-imgs/meme-imgs(square)/${i+1}.jpg" alt="" onclick="onImgSelect(${i+1})">`
+    }
+    strHtmls.unshift('<label for="color-fill" class="color-btn">Upload Image</label> <input type="file" class="color-input" id="file" name="image" onchange="onImgInput(event)" />')
+    document.querySelector('.gallery').innerHTML = strHtmls.join('')
+}
+
+function onImgSelect(idx) {
+    const elEditor = document.querySelector('.edit-meme-layout')
+    elEditor.style.display ='grid'
+    const elGallery = document.querySelector('.gallery')
+    elGallery.style.display = 'none'
+    const elSaved = document.querySelector('.saved-memes')
+    elSaved.style.display = 'none'
+    resizeCanvas()
+    setImg(idx)
+}
 
 
